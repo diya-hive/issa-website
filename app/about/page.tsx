afef4b3837c50ec50bc39ef8a8fc1ce6a1f11460
Diya Krishnakumar <diya@marto.in>
2025-03-11 23:19:45 +0530

    Part 3

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,10 +5,10 @@ import Timeline from "../components/timeline";
 
 export default function About() {
   return (
-    <div className="flex flex-col">
+    <>
       <section className="bg-radial-[at_25%_25%] from-[#1A52A2] to-iblue to-75% z-0">
         <div className="flex flex-col md:flex-row z-10">
-          <div className="flex flex-col gap-8 p-4 md:p-12 lg:p-20 w-screen md:w-[60%] h-screen items-center md:items-start md:justify-center text-white">
+          <div className="flex flex-col gap-8 p-4 md:p-12 lg:p-20 w-screen md:w-[60%] items-center md:items-start md:justify-center text-white">
             <h1 className="font-montesserat-bold text-3xl lg:text-5xl">
               OUR VISION
             </h1>
@@ -22,20 +22,20 @@ export default function About() {
               commitment to global standards.
             </p>
           </div>
-          <div className="flex w-screen md:w-[40%] md:h-screen">
+          <div className="flex w-screen md:w-[40%] h-[30vh] md:h-screen">
             <img src="/AboutHero.png" />
           </div>
         </div>
       </section>
       <section>
         <div className="flex flex-col w-screen justify-center items-center font-montesserat">
-          <h2 className="font-montesserat-bold text-3xl md:text-5xl text-iblue p-6 md:p-12 text-center">
+          <h2 className="font-montesserat-bold text-4xl md:text-5xl text-iblue p-6 md:p-12 text-center">
             OUR JOURNEY
           </h2>
           <Timeline />
         </div>
       </section>
       <CTAOne />
-    </div>
+    </>
   );
 }
