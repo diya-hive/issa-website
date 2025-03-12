dfaa8e5dea49e9ff828e78c726f6cab29769a474
Diya Krishnakumar <diya@marto.in>
2025-03-12 13:45:08 +0530

    Toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,17 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import { Toaster } from "sonner";
+import { mergeOpenGraph } from "@/lib/mergeOpenGraph";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_URL ?? "https://www.issawoodindustries.in"
+  ),
   title: "Issa Wood Industries | Precision Woodcraft & Spools Manufacturing",
   description:
     "With 35 years of expertise, Issa Wood Industries delivers high-quality wooden spools, bobbins, and precision woodcraft solutions. Trusted by leading industries across the country.",
+  openGraph: mergeOpenGraph(),
 };
 
 export default function RootLayout({
@@ -20,6 +26,7 @@ export default function RootLayout({
       <body className={`antialiased`}>
         <Header />
         {children}
+        <Toaster richColors />
         <Footer />
       </body>
     </html>
